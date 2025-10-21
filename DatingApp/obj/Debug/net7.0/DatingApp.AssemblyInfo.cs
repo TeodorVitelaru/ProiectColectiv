50da0773e55b5b378b64246b2bbcda92773883e0
Teodor Vitelaru <teodor.vitelaru@htecgroup.com>
2025-10-21 09:00:57 +0300

    with main

--- DatingApp/obj/Debug/net7.0/DatingApp.AssemblyInfo.cs
+++ DatingApp/obj/Debug/net7.0/DatingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2568adaedb9bd40477f0024454767fc903dfb55")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
