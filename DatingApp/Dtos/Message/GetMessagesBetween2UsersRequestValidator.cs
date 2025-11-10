41288bc35f74f1874e51790d727281ec771c2ab3
Teodor Vitelaru <teodor.vitelaru@htecgroup.com>
2025-11-10 14:55:53 +0200

    added ne method in interface for message service

---
+++ DatingApp/Dtos/Message/GetMessagesBetween2UsersRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp.Dtos.Message;
+using FluentValidation;
+
+namespace DatingApp.Dtos.Message
+{
+    /// <summary>
+    /// Defines validation rules for the <see cref="GetMessagesBetween2UsersRequest"/>
+    /// </summary>
+    public class GetMessagesBetween2UsersRequestValidator : AbstractValidator<GetMessagesBetween2UsersRequest>
+    {
+        public GetMessagesBetween2UsersRequestValidator() 
+        {
+            RuleFor(x => x.FirstUserId).NotEmpty().GreaterThan(0);
+            RuleFor(x => x.SecondUserId).NotEmpty().GreaterThan(0);
+        }
+    }
+}
