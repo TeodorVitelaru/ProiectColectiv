ed8aed27383db591937cf5e36402188f2ca1c2f3
Teodor Vitelaru <teodor_vitelaru@yahoo.com>
2025-11-23 15:33:23 +0200

    added new hobbie configuration

---
+++ DatingApp/Domain/Entities/Hobbie.cs
@@ -0,0 +1,23 @@
+﻿using DatingApp.Domain.Primitives;
+
+namespace DatingApp.Domain.Entities
+{
+    public class Hobbie : Entity<long>
+    {
+        public string HobbieName { get; set; }
+
+        protected Hobbie(long id) : base(id) { }
+
+        protected Hobbie() : base() { }
+
+        public static Hobbie Create(string hobbieName)
+        {
+            Hobbie hobbie = new()
+            {
+                HobbieName = hobbieName
+            };
+
+            return hobbie;
+        }
+    }
+}
